130613ed866fb43958452ac09b4145901c414a18
SpyLap <ballisticc@gmx.de>
2026-08-13 18:41:54 +0200

    Linked Dear ImGUI with SDL2 and setup main ImGUI files live in /include/imgui. Decided to go with SDL2 for the backend, because I had to work with it and do know some of it, even though for this project I probably wont need it.
    CMakeLists is a bit more structured, which was really necessary after
    linking with SDL2 and Dear ImGUI.
    
    Main now opens a window and renders (for now) the ImGUI demo window.
    Everything in main for now is complete boilerplate code so I wont get
    into it.

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,95 @@
+#define SDL_MAIN_HANDLED
 
-int main() {
+#include "imgui.h"
+#include "imgui_impl_sdl2.h"
+#include "imgui_impl_opengl3.h"
 
+#include <SDL.h>
+#include <SDL_opengl.h>
+#include <cstdio>
+
+int main(int, char**)          // (int,char**) sdl entry-point
+{
+    SDL_SetMainReady();
+
+    // --- sdl + opengl context ------------------------------------------
+    if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_TIMER) != 0) {
+        std::printf("SDL_Init error: %s\n", SDL_GetError());
+        return -1;
+    }
+
+    // opengl 3.0 core context
+    const char* glsl_version = "#version 130";
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 0);
+    SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+    SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
+    SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8);
+
+    const auto window_flags = static_cast<SDL_WindowFlags>(
+        SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE | SDL_WINDOW_ALLOW_HIGHDPI);
+
+    SDL_Window* window = SDL_CreateWindow(
+        "StudyTracker",
+        SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+        1280, 720, window_flags);
+    if (window == nullptr) {
+        std::printf("SDL_CreateWindow error: %s\n", SDL_GetError());
+        return -1;
+    }
+
+    SDL_GLContext gl_context = SDL_GL_CreateContext(window);
+    SDL_GL_MakeCurrent(window, gl_context);
+    SDL_GL_SetSwapInterval(1);   // vsync
+
+    // --- Dear IMGUI setup ----------------------------------------------
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO();
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
+    ImGui::StyleColorsDark();
+
+    ImGui_ImplSDL2_InitForOpenGL(window, gl_context);
+    ImGui_ImplOpenGL3_Init(glsl_version);
+
+    bool running = true;
+    while (running) {
+        SDL_Event event;
+        while (SDL_PollEvent(&event)) {
+            ImGui_ImplSDL2_ProcessEvent(&event);
+            if (event.type == SDL_QUIT)
+                running = false;
+            if (event.type == SDL_WINDOWEVENT &&
+                event.window.event == SDL_WINDOWEVENT_CLOSE &&
+                event.window.windowID == SDL_GetWindowID(window))
+                running = false;
+        }
+
+        // new ImGUI frame
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplSDL2_NewFrame();
+        ImGui::NewFrame();
+
+        ImGui::ShowDemoWindow();
+
+        // render -------------------------------------------------------------------------
+        ImGui::Render();
+        glViewport(0, 0, static_cast<int>(io.DisplaySize.x),
+                         static_cast<int>(io.DisplaySize.y));
+        glClearColor(0.45f, 0.55f, 0.60f, 1.00f);
+        glClear(GL_COLOR_BUFFER_BIT);
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+        SDL_GL_SwapWindow(window);
+    }
+
+    // cleanup
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplSDL2_Shutdown();
+    ImGui::DestroyContext();
+
+    SDL_GL_DeleteContext(gl_context);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
     return 0;
 }
